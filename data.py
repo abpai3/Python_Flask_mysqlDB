2fd65f2916b08b6a4d348a00b2ae84e749321be9
Abhishek Pai -X (abpai - WIPRO LIMITED at Cisco) <abpai@cisco.com>
2018-06-18 13:35:14 +0530

    Pyhton sample CRUD app with Flask and MysqlDB

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles=[
+    {
+    'id':1,
+    'title': 'Article One',
+    'Body' : 'first article written in the Python Flask',
+    'author': 'Abhishek',
+    'create_date': '06-02-1994'
+    },
+    {
+    'id':2,
+    'title': 'Article two',
+    'Body' : 'Second article written in the Python Flask',
+    'author': 'Abhishek',
+    'create_date': '06-02-1994'
+    }
+    ]
+    return articles
